9cb7a6e807cf44fa1b61733c6b56bd70fbdf0da4
gabafo1 <gabrielafolabi@gmail.com>
2022-02-11 13:12:40 +0100

    new youtube

---
+++ common/helpers/Html.php
@@ -0,0 +1,14 @@
+<?php 
+
+namespace  common\helpers;
+
+use yii\helpers\Url;
+
+class Html
+{
+    public static function channelLink($user, $scheme = false){
+        return \yii\helpers\Html::a($user->username, 
+        Url::to(['/channel/view', 'username' => $user->username], $scheme), 
+            ['class' => 'text-dark']);
+    }
+}
